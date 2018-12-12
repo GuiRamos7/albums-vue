c011134a723d57801fef40e08ea119ac5e952b22
Guilherme Ramos <GuiOliveiraRamos10@gmail.com>
2018-12-12 02:19:49 -0200

    Creating a hover effect

--- src/components/shared/painel/Panel.vue
+++ src/components/shared/painel/Panel.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="panel">
-    <slot class="panel-content">
-    </slot>
+    <div class="panel-content">
+      <slot></slot>
+    </div>
     <h2 class="panel-title">{{ title }}</h2>
 
   </div>
@@ -25,6 +26,8 @@ export default {
 
   .panel {
     position: relative;
+    overflow: hidden;
+    cursor: pointer;
   }
 
   .panel-title {
@@ -47,16 +50,35 @@ export default {
   .panel-content{
     width: 20em; height: 20em;
     overflow: hidden;
+    position: relative;
   }
 
-  .cover:hover{
-    filter: grayscale(2);
+  .panel:hover .cover{
+    /* filter: grayscale(2); */
+    transform: scale(1.3);
+  }
+
+  .panel-content::after{
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 0px;
+    height: 0px;
+    background-color: #31226491;
+    border-radius: 50%;
+    transition: .4s ease-in-out;
+  }
+
+  .panel:hover .panel-content::after {
+    width: 150%;
+    height: 150%;
   }
 
   .cover {
     transition: .3s ease-in-out;
-    z-index: -1;
-    width: 100%
+    width: 100%;
   }
 
 </style>
